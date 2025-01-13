16071c431ff5194152eed46322b341ab8ec028ef
Raiyan Ahmed <ray38250968@gmail.com>
2025-01-13 19:26:43 +0530

    Feat(): added ReactRouterV7 with simple test routing sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-
+import { Routes, Route } from "react-router";
 import MovieContextProvider from '@/utils/MovieContextProvider'
+import { Outlet } from "react-router"; // to remove
 function App() {
 
   return (
     <>
       <MovieContextProvider>
-        <h1>Home</h1>
-        <h1>Search</h1>
+        <Routes>
+          <Route index element={<h1>Home</h1>} />
+          <Route path='test' element={<h1>test <br /> <Outlet /> </h1>}>
+            <Route path='search' element={<h1>Search</h1>} />
+          </Route>
+        </Routes>
       </MovieContextProvider>
     </>
   )
